9af4fef003f75b16431aa96381270e97cd59e034
notaskhalsa <askhalsa29@gmail.com>
2025-06-23 23:05:06 -0400

    UI: add style for preline prompt

---
+++ portfolio/src/components/Prompt.jsx
@@ -0,0 +1,12 @@
+
+const Prompt = () => {
+  return (
+    <div className="flex items-center gap-1 flex-wrap">
+      <span className="text-green-400">askhalsa@dev</span>
+      <span className="text-yellow-400">(main)</span>
+      <span className="text-white">$</span>
+    </div>
+  );
+};
+
+export default Prompt;
